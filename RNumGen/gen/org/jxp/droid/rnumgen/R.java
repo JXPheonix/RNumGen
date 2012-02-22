9c2e07d022d1c0c208c62785d6378cab93a180e9
Kyle Fahringer <jxpheonix@gmail.com>
2012-02-22 18:02:01 -0500

    a little bit of work still trying to figure out dynamic xml strings more work tomoro

--- RNumGen/gen/org/jxp/droid/rnumgen/R.java
+++ RNumGen/gen/org/jxp/droid/rnumgen/R.java
@@ -18,6 +18,7 @@ public final class R {
         public static final int content=0x7f050001;
         public static final int handle=0x7f050002;
         public static final int slidingDrawer1=0x7f050000;
+        public static final int textView1=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -26,6 +27,7 @@ public final class R {
         public static final int app_name=0x7f040001;
         public static final int description=0x7f040003;
         public static final int hello=0x7f040000;
+        public static final int number=0x7f040005;
         public static final int pulltogen=0x7f040004;
         public static final int version=0x7f040002;
     }
