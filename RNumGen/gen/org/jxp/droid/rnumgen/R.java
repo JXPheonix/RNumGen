1014f55e3be66d8ef4627dc3ca96139ce7e56c02
Kyle Fahringer <jxpheonix@gmail.com>
2012-02-22 17:29:14 -0500

    temp logo whipped up in ms paint and some graphics

--- RNumGen/gen/org/jxp/droid/rnumgen/R.java
+++ RNumGen/gen/org/jxp/droid/rnumgen/R.java
@@ -12,13 +12,21 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int rnumgen_logo=0x7f020001;
+    }
+    public static final class id {
+        public static final int content=0x7f050001;
+        public static final int handle=0x7f050002;
+        public static final int slidingDrawer1=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int description=0x7f040003;
         public static final int hello=0x7f040000;
+        public static final int pulltogen=0x7f040004;
         public static final int version=0x7f040002;
     }
 }
