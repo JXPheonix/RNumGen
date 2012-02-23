a0a452aa299a4b5192936e17107aff675847de54
Kyle Fahringer <jxpheonix@gmail.com>
2012-02-22 20:13:19 -0500

    actually working but no mechanisims to choose range or seed but a working one point zero release i think yay

--- RNumGen/gen/org/jxp/droid/rnumgen/R.java
+++ RNumGen/gen/org/jxp/droid/rnumgen/R.java
@@ -18,7 +18,7 @@ public final class R {
         public static final int content=0x7f050001;
         public static final int handle=0x7f050002;
         public static final int slidingDrawer1=0x7f050000;
-        public static final int textView1=0x7f050003;
+        public static final int textViewNum=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- RNumGen/src/org/jxp/droid/rnumgen/RNumGenActivity.java
+++ RNumGen/src/org/jxp/droid/rnumgen/RNumGenActivity.java
@@ -2,6 +2,8 @@ package org.jxp.droid.rnumgen;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import java.util.Random;
 
 public class RNumGenActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,14 @@ public class RNumGenActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        TextView numtv = (TextView) findViewById(R.id.textViewNum);
+        numtv.setText(gen() + "!");
     }
+
+	private String gen() {
+		Random randomizer = new Random();
+		int randomnum = randomizer.nextInt();
+		String result = Integer.toString(randomnum);
+		return result;
+	}
 }
